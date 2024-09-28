d2f833cb31361dcbaebb08e753f89ce3426609b9
Javier Gonzalez <javgon2306@gmail.com>
2024-09-27 23:38:35 -0300

    add db.sql

---
+++ database/db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (name, email) VALUES ('Javier', 'javier@gmail.com');
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,5 +5,19 @@ const router = Router();
 router.get('/users', (req, res) => {
     res.send('Obteniendo usuarios')
 })
+router.get('/users/:id', (req, res) => {
+    const {id} = req.params
+    res.send('Obteniendo usuario' + id)
+})
+router.post('/users', (req, res) => {
+    res.send('Creando usuario')
+})
+router.delete('/users/:id', (req, res) => {
+
+    res.send('Eliminando usuario')
+})
+router.put('/users/:id', (req, res) => {
+    res.send('Actualizando usuario')
+})
 
 export default router;
