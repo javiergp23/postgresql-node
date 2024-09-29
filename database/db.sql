f3f8e17223df85ab0afa02d2c41c2157e283bea8
Javier Gonzalez <javgon2306@gmail.com>
2024-09-29 17:45:55 -0300

    db

--- database/db.sql
+++ database/db.sql
@@ -5,4 +5,19 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
 INSERT INTO users (name, email) VALUES ('Javier', 'javier@gmail.com');
