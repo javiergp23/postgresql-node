539bfae8f68925d57ced3442d5ce85c869f40886
Javier Gonzalez <javgon2306@gmail.com>
2024-10-01 00:35:06 -0300

    create pool

--- database/db.sql
+++ database/db.sql
@@ -1,11 +1,7 @@
 CREATE TABLE users (
-    
     id serial PRIMARY KEY,
-
     name varchar(255) NOT NULL,
-
     email varchar(255) NOT NULL,
-
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- src/db.js
+++ src/db.js
@@ -1,1 +1,15 @@
-//DATABASe
+import pg from 'pg';
+
+export const pool = new pg.Pool(
+    {
+        user: 'postgres',
+        host: 'localhost',
+        password: '060923',
+        database: 'nodepg',
+        port: 5432,
+    }
+)
+
+pool.query('SELECT NOW()').then(result => {
+    console.log(result)
+})
